0fa85895981366e950fafca4a7786b2e72194f1a
Will Richardson <william.hamish@gmail.com>
2023-06-08 08:05:37 +0000

    Add some basic tests

--- spec/pod_spec.cr
+++ spec/pod_spec.cr
@@ -1,6 +1,41 @@
 require "./spec_helper"
 
-describe Pod do
-  it "works" do
+describe Pod::Runner do
+  it "builds" do
+    result = assert_runner(config(
+      "images:
+        build_test:
+          from: Containerfile.test
+          tag: my-image:latest
+      "
+    )) do |runner|
+      runner.build "build_test"
+    end
+    result[0].should eq("podman build --tag=my-image:latest --file=Containerfile.test .")
+  end
+
+  it "runs" do
+    result = assert_runner(config(
+      "containers:
+         run_test:
+           name: some-container
+           image: my-image:latest
+           bind_mounts:
+             .: /src
+           ports:
+             1234: 80
+           interactive: true
+           autoremove: true
+           flags:
+             some-flag: 'some value'
+      "
+    )) do |runner|
+      runner.run "run_test", nil, nil
+    end
+    result[0].should eq(
+      "podman run --tty=true --interactive=true --rm=true " +
+      "--mount=type=bind,src=.,dst=/src --publish=1234:80 --name=some-container " +
+      "--hostname=some-container --label=pod_hash=85d2a8935e49493cbf7efd893d993c531e04a31e" +
+      " my-image:latest '--some-flag=some value'")
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,12 @@
 require "spec"
-require "../src/pod-compost"
+require "../src/pod/*"
+
+def config(str)
+  Pod::Config::File.from_yaml(str)
+end
+
+def assert_runner(config : Pod::Config::File)
+  String.build do |io|
+    yield Pod::Runner.new(config, nil, show: true, io: io)
+  end.split('\n')
+end
